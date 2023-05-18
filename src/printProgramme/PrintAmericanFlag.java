9d8f665f7e68ba3351489bc72c26c46d9dd667c7
mp775 <manyapatel775@gmail.com>
2023-05-18 22:43:09 +0100

    Resubmission Java Homework Week 1

---
+++ src/printProgramme/PrintAmericanFlag.java
@@ -0,0 +1,27 @@
+package printProgramme;
+
+public class PrintAmericanFlag {
+    /**
+     * 6. Write a Java program to print an American flag on the screen.
+     */
+    public static void main(String[] args) {
+
+
+        System.out.println("* * * * * * ===================================");
+        System.out.println(" * * * * *  ===================================");
+        System.out.println("* * * * * * ===================================");
+        System.out.println(" * * * * *  ===================================");
+        System.out.println("* * * * * * ===================================");
+        System.out.println(" * * * * *  ===================================");
+        System.out.println("* * * * * * ===================================");
+        System.out.println(" * * * * *  ===================================");
+        System.out.println("* * * * * * ===================================");
+        System.out.println("===============================================");
+        System.out.println("===============================================");
+        System.out.println("===============================================");
+        System.out.println("===============================================");
+        System.out.println("===============================================");
+        System.out.println("===============================================");
+    }
+
+}
---
+++ src/printProgramme/PrintScaleneTriangle.java
@@ -0,0 +1,26 @@
+package printProgramme;
+
+public class PrintScaleneTriangle {
+
+    /**
+     * 4. Write a Java Program to print below triangle
+     *
+     * *
+     * **
+     * ***
+     * ****
+     * *****
+     * ******
+     */
+
+        public static void main(String[] args) {
+            System.out.println("*");
+            System.out.println("**");
+            System.out.println("***");
+            System.out.println("****");
+            System.out.println("*****");
+            System.out.println("******");
+        }
+    }
+
+
